6bb75ab60dda7cb6e52aa3808c07de81e05cdfcf
QPomelo <bakaborange@gmail.com>
2020-03-14 13:54:32 +0800

    Initial Commit

---
+++ SimpleBillMgr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleBillMgr
+//
+//  Created by Pomelo on 14/3/2020.
+//  Copyright © 2020 QPomelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
